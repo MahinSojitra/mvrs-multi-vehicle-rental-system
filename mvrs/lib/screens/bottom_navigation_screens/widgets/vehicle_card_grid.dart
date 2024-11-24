d32ef20ffa29b4c518e49feedff8d0d0ba9b66fb
MahinSojitra <mahin.sojitra@outlook.com>
2024-11-24 14:21:26 +0530

    change vehicle_card_grid.dart

--- mvrs/lib/screens/bottom_navigation_screens/widgets/vehicle_card_grid.dart
+++ mvrs/lib/screens/bottom_navigation_screens/widgets/vehicle_card_grid.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:mvrs/screens/bottom_navigation_screens/widgets/vehicle_card.dart';
 
@@ -7,61 +9,61 @@ class VehicleCardGrid extends StatelessWidget {
       'name': 'Tesla Model S',
       'imageUrl':
           'https://hips.hearstapps.com/hmg-prod/images/2024-tesla-model-s-107-6572200e43fa1.jpg?crop=0.497xw:0.560xh;0.243xw,0.232xh&resize=768:*',
-      'rent': 120,
+      'rent': 120.0,
     },
     {
       'name': 'BMW i8',
       'imageUrl':
           'https://www.shutterstock.com/image-photo/kuala-lumpur-malaysia-april-7-600nw-2143397457.jpg',
-      'rent': 200,
+      'rent': 200.0,
     },
     {
       'name': 'Thar',
       'imageUrl':
           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpBe_O3wM7b3hfln5TYnbfTO7QpabGBt4w_Q&s',
-      'rent': 90,
+      'rent': 90.0,
     },
     {
       'name': 'Nissan GT-R',
       'imageUrl':
           'https://cdni.autocarindia.com/Utils/ImageResizer.ashx?n=https://cdni.autocarindia.com/ExtraImages/20240628064514_Untitled%20design%20_%202024_06_28T184501.816.jpg',
-      'rent': 150,
+      'rent': 150.0,
     },
     {
       'name': 'Ford Mustang',
       'imageUrl':
           'https://upload.wikimedia.org/wikipedia/commons/7/7f/1965_Ford_Mustang_2D_Hardtop_Front.jpg',
-      'rent': 180,
+      'rent': 180.0,
     },
     {
       'name': 'Chevrolet',
       'imageUrl':
           'https://images.thequint.com/thequint%2F2016-02%2F15ee3c7d-b1d0-4354-a635-30d0f51d828f%2FCruze-hero.jpg',
-      'rent': 170,
+      'rent': 170.0,
     },
     {
       'name': 'Porsche 911',
       'imageUrl':
           'https://assets.porsche.com/dwaas-infohub/-/media/Project/DealerWebsites/SharedDealersWebsite/Master-Model-Page/911-Turbo/General-Model/General-model_911-Turbo.jpg?rev=5efc15cd5f68412c8ed778e0c199ed16',
-      'rent': 300,
+      'rent': 300.0,
     },
     {
       'name': 'Lamborghini Huracan',
       'imageUrl':
           'https://stimg.cardekho.com/images/carexteriorimages/930x620/Lamborghini/Huracan-EVO/10643/1690010999692/front-left-side-47.jpg',
-      'rent': 500,
+      'rent': 500.0,
     },
     {
       'name': 'Ferrari 488',
       'imageUrl':
           'https://sbraceengineering.co.uk/wp-content/uploads/Ferrari-488-GTB.jpg',
-      'rent': 450,
+      'rent': 450.0,
     },
     {
       'name': 'Maserati Ghibli',
       'imageUrl':
           'https://imgd-ct.aeplcdn.com/664x415/n/j0irpsa_1476670.jpg?q=80',
-      'rent': 220,
+      'rent': 220.0,
     },
   ];
 
